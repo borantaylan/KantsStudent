439720f509fcd07b515c518de1a030e93b3073c3
borantaylan <borantaylan@hotmail.com>
2017-10-04 12:25:05 +0200

    v3

--- apps/alexa-kantsstudent/index.js
+++ apps/alexa-kantsstudent/index.js
@@ -6,7 +6,7 @@ const verifier = require('alexa-verifier');
 var app = new alexa.app('kantsstudent');
 const questions=[
   "how would you define human beings perception of objects from a philosophical point<break time=\"1s\"/>",
-  "You mean time and space",
+  "You mean time and space<break time=\"1s\"",
   "So you say, people are genetically programmed to develop without having to rely on experience. If humans see the things through these tools of understanding, how would they have knowledge of the real world",
   "Then it brings solipsism. The self is the only reality.",
   "So, people see the world from a spectacle and this spectacle can not be removed since they are part of the visual equipment. People will receive signals from outside through the apparatus which will distort the signals naturally. But then how humans can know there is really something that is sending signals ?",
